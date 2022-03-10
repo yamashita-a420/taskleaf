8bce569a1073af31872650639192a11403427f1e
yamashita-a420 <ayanonagai420@gmail.com>
2022-03-10 11:20:51 +0900

    Update:各種機能、ストロングパラメータの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,47 +1,47 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show,  :edit, :update, :destroy]
+
   def index
-    byebug # 1個目
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def new
-    byebug # 2個目
-    @tasks = Task.all
     @task = Task.new
   end
 
   def create
-    byebug # 3個目
-    task = Task.new(task_params)
-    byebug # 4個目
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = current_user.tasks.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
+    @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
+    @task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
   
   private
 
   def task_params
-    byebug # 5個目
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
